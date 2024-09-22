0b4b24f2c3c112d7a55e317bc90881cfcda33a74
Dylan-xu <dylanxu473@gmail.com>
2024-09-22 14:50:42 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://https://www.421808.xyz/' },
+    { title: '欢迎来到我的博客', url: 'https://www.421808.xyz/' },
     { title: '点我及时获取最新有用、有趣的资源', url: 'https://www.421808.xyz/' }
   ],
 
